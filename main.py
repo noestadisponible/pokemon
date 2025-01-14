fc923075a089c740c0a9eedb20847c74132c53c9
noestadisponible <lucho.salas@educa.madrid.org>
2025-01-14 14:01:12 +0100

    pokemon

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI()
 
-# Montar carpeta de archivos estáticos
+# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Configurar plantillas
+# Set up templates
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def read_home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/equipos", response_class=HTMLResponse)
-async def equipos(request: Request):
-    return templates.TemplateResponse("equipos.html", {"request": request})
+@app.get("/pokedex", response_class=HTMLResponse)
+async def read_pokedex(request: Request):
+    return templates.TemplateResponse("pokedex.html", {"request": request})
 
-@app.get("/estadisticas", response_class=HTMLResponse)
-async def estadisticas(request: Request):
-    return templates.TemplateResponse("estadisticas.html", {"request": request})
+@app.get("/trainers", response_class=HTMLResponse)
+async def read_trainers(request: Request):
+    return templates.TemplateResponse("trainers.html", {"request": request})
+
+@app.get("/battles", response_class=HTMLResponse)
+async def read_battles(request: Request):
+    return templates.TemplateResponse("battles.html", {"request": request})
