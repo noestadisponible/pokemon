400e758f29da7f367b0c12bb9f5c3ef6816647cd
noestadisponible <lucho.salas@educa.madrid.org>
2025-01-16 09:48:18 +0100

    videos

--- main.py
+++ main.py
@@ -25,5 +25,24 @@ async def read_trainers(request: Request):
     return templates.TemplateResponse("trainers.html", {"request": request})
 
 @app.get("/battles", response_class=HTMLResponse)
-async def read_battles(request: Request):
-    return templates.TemplateResponse("battles.html", {"request": request})
+async def battles(request: Request):
+    battle_links = [
+        {
+            "title": "Battle 1: Pikachu vs Charizard",
+            "url": "https://youtu.be/7nDehONtCq4",
+            "video_id": "7nDehONtCq4"
+        },
+        {
+            "title": "Battle 2: Charmander vs Agumon",
+            "url": "https://youtu.be/lfqVW-xWX0g?si=Y2gamOyr3HOXxiLK",
+            "video_id": "lfqVW-xWX0g"
+        },
+        {
+            "title": "Battle 3: Bulbasaur vs Skiploom",
+            "url": "https://youtu.be/rqPfQgOEEeo?si=XR1Z4iAz2MQnSgj7",
+            "video_id": "rqPfQgOEEeo"
+        }
+    ]
+    return templates.TemplateResponse("battles.html", {"request": request, "battle_links": battle_links})
+
+
